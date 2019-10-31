62c1ad3d7cf46e15bbaed4311bd984f96626fca4
Mayur Patil <mayurpatil001@hotmail.com>
2019-11-01 00:10:29 +0530

    Initial Commit till Part -05

---
+++ main.lua
@@ -0,0 +1,100 @@
+
+local push = require "libs/push"
+
+Class = require "libs/class"
+
+require "Paddle"
+
+require "Ball"
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+PADDLE_SPEED = 300
+
+function love.load()
+
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+
+  math.randomseed(os.time())
+
+  smallFont = love.graphics.newFont("fonts/font.ttf", 8)
+  scoreFont = love.graphics.newFont("fonts/font.ttf", 32)
+
+  love.graphics.setFont(smallFont)
+
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    resizable = false,
+    vsync = true
+  })
+
+  player1Score = 0
+  player2Score = 0
+
+  player1 = Paddle(10, 30, 5, 20)
+  player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+
+  ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+  gameState = 'start'
+end
+
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit()
+  elseif key == 'enter' or key == 'return' then
+    if gameState == 'start' then
+      gameState = 'play'
+    else
+      gameState = 'start'
+
+      ball:reset()
+      
+    end
+  end
+end
+
+function love.update(dt)
+  if love.keyboard.isDown('w') then
+    player1.dy = -PADDLE_SPEED
+  elseif love.keyboard.isDown('s') then
+    player1.dy = PADDLE_SPEED
+  end
+
+  if love.keyboard.isDown('up') then
+    player2.dy = -PADDLE_SPEED
+  elseif love.keyboard.isDown('down') then
+    player2.dy = PADDLE_SPEED
+  end
+
+  if gameState == 'play' then
+    ball:update(dt)
+  end
+
+  player1:update(dt)
+  player2:update(dt)
+end
+
+function love.draw()
+  push:start()
+  love.graphics.clear(40  / 255, 45 / 255, 52 / 255, 255 / 255)
+
+  love.graphics.setFont(smallFont)
+  love.graphics.printf("Hello Pong!", 0, 20, VIRTUAL_WIDTH, 'center')
+
+  love.graphics.setFont(scoreFont)
+  love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+  love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
+  player1:render()
+
+  player2:render()
+
+  ball:render()
+
+  push:finish()
+end
